73647c06aa2a99b2c8de0f108a7a1e352d8c6fb6
ricardogzm <ricardogzm.dev@gmail.com>
2022-05-18 11:59:10 -0500

    🎨 Transparent system menu bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/services.dart';
+
 import 'src/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +9,15 @@ import 'package:pizca_de_sal/src/routes_generator.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemStatusBarContrastEnforced: true,
+    systemNavigationBarColor: Colors.white.withOpacity(0.8),
+    systemNavigationBarContrastEnforced: true,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
 
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => ThemeManager()),
@@ -24,6 +35,7 @@ class MyApp extends StatelessWidget {
       themeMode: context.watch<ThemeManager>().currentTheme,
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
